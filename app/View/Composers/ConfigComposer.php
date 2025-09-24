418036b51c487e8c8890b55f212f6cf13acd4225
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-24 16:14:37 +0700

    fix : app description on sign up

--- app/View/Composers/ConfigComposer.php
+++ app/View/Composers/ConfigComposer.php
@@ -12,7 +12,8 @@ class ConfigComposer
         $view->with([
             'appName' => Config::get('app_name', 'My Application'),
             'appLogo' => Config::get('app_logo', '/assets/media/logos/default-logo.png'),
-            'legalName' => Config::get('legal_name', '/assets/media/logos/default-logo.png'),
+            'legalName' => Config::get('legal_name', 'Legal Name'),
+            'appDescription' => Config::get('app_description', 'Description'),
         ]);
     }
 }
--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -94,12 +94,8 @@ License: For each use you must have a valid license purchased only from above li
                     <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">{{ $appName ?? 'My Application' }}</h1>
                     <!--end::Title-->
                     <!--begin::Text-->
-                    <div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
-                        <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person
-                        they’ve interviewed
-                        <br />and provides some background information about
-                        <a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>and their
-                        <br />work following this is a transcript of the interview.
+                    <div class="text-gray-600 fs-base text-center fw-semibold">
+                        {{ $appDescription ?? 'My Application' }}
                     </div>
                     <!--end::Text-->
                 </div>
