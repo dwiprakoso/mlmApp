02451c82190a29469aef25cc5cb225170af36880
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-24 16:29:28 +0700

    fix : index view withdraw controller

--- app/Http/Controllers/Admin/WithdrawController.php
+++ app/Http/Controllers/Admin/WithdrawController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class WithdrawController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.withdraw.index');
+        $withdrawTransactions = Transaction::where('type', 'withdraw')
+            ->with(['user', 'wallet'])
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('admin.pages.withdraw.index', compact('withdrawTransactions'));
     }
 }
