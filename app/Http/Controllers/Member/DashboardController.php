fa7e04df3e508cfe5be74bcc708dfa6fc9504bfc
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-24 19:14:39 +0700

    fix : balance on index member

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -15,26 +15,20 @@ class DashboardController extends Controller
         $referralCode = $user->refferal_code;
         $referralLink = route('guest.sign-up', ['ref' => $referralCode]);
 
-
-
-        // Hitung total deposit yang sukses
-        $totalDeposit = Transaction::where('user_id', $user->id)
-            ->where('type', 'deposit')
-            ->where('status', 'success')
-            ->sum('amount');
-
-        // Hitung total withdrawal yang sudah sukses atau pending
-        $totalWithdraw = Transaction::where('user_id', $user->id)
-            ->where('type', 'withdraw')
-            ->whereIn('status', ['success', 'pending'])
-            ->sum('amount');
-        $balance = $totalDeposit - $totalWithdraw;
+        $balance = Transaction::calculateUserBalance($user->id);
 
         $commissionRate = Config::where('key', 'team_invite_presentation')
             ->value('value') ?? '10';
         $headerText = Config::where('key', 'header_text')
             ->value('value') ?? '10';
 
-        return view('member.pages.dasboard.index', compact('user', 'referralCode', 'referralLink', 'balance', 'commissionRate', 'headerText'));
+        return view('member.pages.dasboard.index', compact(
+            'user',
+            'referralCode',
+            'referralLink',
+            'balance',
+            'commissionRate',
+            'headerText'
+        ));
     }
 }
