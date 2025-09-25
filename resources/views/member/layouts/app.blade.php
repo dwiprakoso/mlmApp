62f4a565e8347be2109de10e2d427054f077585d
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-25 13:27:50 +0700

    fix : link icon

--- resources/views/member/layouts/app.blade.php
+++ resources/views/member/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <base href="{{ url('/') }}/" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Member Dashboard</title>
+    <link rel="icon" href="{{ $appLogo ?? 'https://via.placeholder.com/40' }}">
+    <title>{{ $appName ?? 'My Application' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
