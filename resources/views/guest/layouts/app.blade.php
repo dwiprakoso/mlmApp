efabfa2a0e75a624b1185713f88a432c5c19efe9
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-25 13:30:35 +0700

    fix : icon tab browser

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -18,17 +18,19 @@ License: For each use you must have a valid license purchased only from above li
     <title>{{ $appName ?? 'My Application' }}</title>
     <meta charset="utf-8" />
     <meta name="description"
-        content="The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
+        content="{{ $appName ?? 'My Application' }} - Platform investasi terpercaya untuk masa depan yang lebih baik." />
     <meta name="keywords"
-        content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+        content="{{ $appName ?? 'My Application' }}, investasi, platform investasi, financial technology, fintech" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic - The World's #1 Selling Bootstrap Admin Template by KeenThemes" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Metronic by Keenthemes" />
-    <link rel="canonical" href="http://authentication/layouts/overlay/sign-up.html" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <meta property="og:locale" content="id_ID" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{{ $appName ?? 'My Application' }} - Platform Investasi Terpercaya" />
+    <meta property="og:description"
+        content="{{ $appName ?? 'My Application' }} - Platform investasi terpercaya untuk masa depan yang lebih baik." />
+    <meta property="og:url" content="{{ url('/') }}" />
+    <meta property="og:site_name" content="{{ $appName ?? 'My Application' }}" />
+    <link rel="canonical" href="{{ url('/') }}" />
+    <link rel="shortcut icon" href="{{ $appLogo ?? 'https://via.placeholder.com/40' }}" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
