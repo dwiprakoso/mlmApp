21d0e9cc7e7b0bcdf1e8232eb842208cad98a648
dwiprakoso <dwiprakoso015@gmail.com>
2025-09-25 13:19:15 +0700

    fix : dashboard index view

--- resources/views/member/pages/dasboard/index.blade.php
+++ resources/views/member/pages/dasboard/index.blade.php
@@ -36,7 +36,7 @@
         <h6 class="text-white mb-3">Bantuan & Perawatan</h6>
         <div class="row text-center">
             <div class="col">
-                <a href="https://wa.me/{{ $whatsappNumber }}" class="text-decoration-none">
+                <a href="https://wa.me/{{ $whatsappNumber }}" class="text-decoration-none" target="_blank">
                     <div class="circle-icon bg-primary text-white">
                         <i class="bi bi-headset"></i>
                     </div>
